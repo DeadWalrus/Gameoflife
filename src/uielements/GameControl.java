3a0d2918af1d9323a7f940a618aff70c89cf849d
DeadWalrus <pointlesstack@gmail.com>
2023-01-14 20:41:58 -0500

    add listener to combobox to switch game type

--- src/uielements/GameControl.java
+++ src/uielements/GameControl.java
@@ -5,10 +5,9 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.util.Duration;
+
 
 
 public class GameControl extends HBox {
@@ -25,29 +24,16 @@ public class GameControl extends HBox {
         this.gameType = 1;
         initButtons();
         initComboBox();
+        initAnimation();
         getChildren().add(this.btPlay);
         getChildren().add(this.btClear);
         getChildren().add(this.cbGameType);
-        this.tlGameLoop = new Timeline();
-        this.tlGameLoop.setCycleCount(Timeline.INDEFINITE);
     }
 
     private void initButtons(){
         this.btPlay = new Button("Play/Pause");
 
         this.btPlay.setOnAction(e -> {
-            if (this.cbGameType.getSelectionModel().getSelectedItem().equals("Default")) {
-                this.tlGameLoop.stop();
-                this.tlGameLoop.getKeyFrames().clear();
-                this.tlGameLoop.getKeyFrames().add(new KeyFrame(Duration.millis(100), actionEvent -> gameBoard.updateBoard((byte) 1)));
-                this.tlGameLoop.play();
-            } else {
-                this.tlGameLoop.stop();
-                this.tlGameLoop.getKeyFrames().clear();
-                this.tlGameLoop.getKeyFrames().add(new KeyFrame(Duration.millis(100), actionEvent -> gameBoard.updateBoard((byte) 2)));
-                this.tlGameLoop.play();
-            }
-
             this.gameBoard.setAnimate(!this.gameBoard.getAnimate());
         });
         this.btClear = new Button("Clear");
@@ -58,5 +44,22 @@ public class GameControl extends HBox {
         this.cbGameType = new ComboBox<>();
         this.cbGameType.getItems().addAll("Default", "Growth");
         this.cbGameType.getSelectionModel().selectFirst();
+        this.cbGameType.getSelectionModel().selectedItemProperty().addListener((observableValue, oldVal, newVal) ->{
+            switch (newVal) {
+                case "Default" -> gameBoard.setGameType((byte) 1);
+                case "Growth" -> gameBoard.setGameType((byte) 2);
+                default -> {
+                    System.out.println("Game type not valid");
+                    gameBoard.setGameType((byte) 1);
+                }
+            }
+        });
+    }
+
+    private void initAnimation(){
+        this.tlGameLoop = new Timeline();
+        this.tlGameLoop.setCycleCount(Timeline.INDEFINITE);
+        this.tlGameLoop.getKeyFrames().add(new KeyFrame(Duration.millis(100), actionEvent -> this.gameBoard.updateBoard()));
+        this.tlGameLoop.play();
     }
 }
